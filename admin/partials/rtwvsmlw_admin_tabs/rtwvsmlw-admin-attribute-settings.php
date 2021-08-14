74b484d32e17e77c6593f3168450300e5fed71e6
redefiningtheweb <46479644+redefiningtheweb@users.noreply.github.com>
2021-08-14 23:03:21 +0530

    removed iframe

--- admin/partials/rtwvsmlw_admin_tabs/rtwvsmlw-admin-attribute-settings.php
+++ admin/partials/rtwvsmlw_admin_tabs/rtwvsmlw-admin-attribute-settings.php
@@ -15,6 +15,7 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
 <div class='rtwvsmlw_attribute_iframe'>
-    <iframe id='rtwvsmlw_attribute_iframe_url' src="<?php echo isset($_SESSION['rtwvsmlw_attr_redirect_url']) ? esc_url($_SESSION['rtwvsmlw_attr_redirect_url']) : esc_url(admin_url().'edit.php?post_type=product&page=product_attributes&rtwvsmlw_variation_swatches=rtwvsmlw_yes') ?>">
-    </iframe>
+    <img id="rtwvsmlw_attr_imgges" src="<?php echo esc_url(RTWVSMLW_URL.'assets/variation-attribute-setting.png'); ?>"/>
+    <!-- <iframe id='rtwvsmlw_attribute_iframe_url' src="<?php //echo isset($_SESSION['rtwvsmlw_attr_redirect_url']) ? esc_url($_SESSION['rtwvsmlw_attr_redirect_url']) : esc_url(admin_url().'edit.php?post_type=product&page=product_attributes&rtwvsmlw_variation_swatches=rtwvsmlw_yes') ?>">
+    </iframe> -->
 </div>
